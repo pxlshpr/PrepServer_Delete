f6392164929424eb6248ee5baff5ba5f208d3226
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-24 14:48:31 +0500

    bump

--- Sources/App/Migrations/CreateMeal.swift
+++ Sources/App/Migrations/CreateMeal.swift
@@ -5,7 +5,7 @@ struct CreateMeal: AsyncMigration {
         try await database.schema("meals")
             .id()
             .field("day_id", .string, .references(Day.schema, .id), .required)
-            .field("goal_set_id", .string, .references(GoalSet.schema, .id))
+            .field("goal_set_id", .uuid, .references(GoalSet.schema, .id))
             .field("created_at", .double, .required)
             .field("updated_at", .double, .required)
             .field("deleted_at", .double)
