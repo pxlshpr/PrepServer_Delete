9ed86e685835ef42c68195127525bf28a535b2cb
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-05 18:59:58 +0500

    bump

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ func routes(_ app: Application) throws {
     try app.register(collection: SyncController())
     
     app.get("hello") { req in
-        try shell("pg_dump -U pxlshpr prep > ~/prep-backup-\(Date().timestamp).sql")
+        try shell("pg_dump", "-U", "pxlshpr", "prep", ">", "~/prep-backup-\(Date().timestamp).sql")
         return "We here"
     }
 }
