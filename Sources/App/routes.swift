347a661fb128a31d703aab2929347a5d4317fbdd
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-05 19:19:16 +0500

    bump

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,8 +4,8 @@ import Vapor
 func routes(_ app: Application) throws {
     try app.register(collection: SyncController())
     
-    app.get("hello") { req in
+    app.get("backup") { req in
         try shell("./backup.sh")
-        return "We here"
+        return HTTPStatus.ok
     }
 }
