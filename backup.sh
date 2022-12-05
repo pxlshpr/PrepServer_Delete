0f8e3c6fb3e23aff6f8151137ee4df220133e777
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-05 19:14:24 +0500

    bump

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 unixtime=$(date +%s)
 file="/home/ahmed/PrepBackups/prep-backup-${unixtime}.sql"
 pg_dump -U pxlshpr prep > $file
-echo "             💾 Backup saved to: ${file}"
+echo "         💾 Backup saved to: ${file}"
+cd /home/ahmed/PrepBackups
+pwd
